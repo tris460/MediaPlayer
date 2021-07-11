08ce6a041933dde6ccae026ba6ff2344c865ba01
Beatriz Martinez <trism@gmail.com>
2021-07-11 14:26:10 -0500

    Funcionalidad AutoPause completa

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './Plugins/AutoPlay.js';
 import PlayList from './Plugins/PlayList.js';
+import AutoPause from './Plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const buttonPlay = document.querySelector('button[id="Play"]');
 const buttonMute = document.querySelector('button[id="Mute"]');
 const buttonNext = document.querySelector('button[id="Next"]');
-// const selector = document.querySelector('select');
 const player = new MediaPlayer({el:video,
-                                plugins:[new AutoPlay()], 
+                                plugins:[new AutoPlay(), new AutoPause()], 
                                 song:[new PlayList()]});
  
 buttonPlay.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.volumen();
-// selector.onchange = () => player.nextSong();
 buttonNext.onclick = () => player.nextSong();
