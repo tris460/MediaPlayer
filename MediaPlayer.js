2585603421d9cf4a2fd8a78a079f86e74fd91c8c
Beatriz Martinez <trism@gmail.com>
2021-07-13 22:50:02 -0500

    Tipado de AutoPlay a TS

--- MediaPlayer.js
+++ MediaPlayer.js
@@ -7,19 +7,20 @@ class MediaPlayer {
 
     }
     _initPlugins() {
-        const player = {
-            play: () => this.play(),
-            pause: () => this.pause(),
-            media: this.media,
-            get muted() {
-                return this.media.muted;
-            },
-            set muted(value) {
-                this.media.muted = value;
-            }
-        };
+        // const player = {
+        //     play: () => this.play(),
+        //     pause: () => this.pause(),
+        //     media: this.media,
+        //     get muted() {
+        //         return this.media.muted;
+        //     },
+        //     set muted(value) {
+        //         this.media.muted = value;
+        //     }
+        // };
         this.plugins.forEach(plugin => {
-            plugin.run(player);
+            plugin.run(this)
+            //plugin.run(player);
         });
     }
     play() {
